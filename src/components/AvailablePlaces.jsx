3e7b3057a53a578c6fa3e774278c3a2e99a90106
EslamKamel89 <elsamkamelforex@gmail.com>
2024-11-23 20:29:43 +0200

    adding sorting by geolocation feature

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,55 @@
-import Places from './Places.jsx';
-
+import { useEffect, useState } from "react";
+import { sortPlacesByDistance } from "../loc.js";
+import Places from "./Places.jsx";
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+      try {
+        var response = await fetch("http://localhost:3000/places");
+        if (!response.ok) {
+          const error = new Error("Failed to fetch places data");
+          throw error;
+        }
+        var resData = await response.json();
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+      } catch (error) {
+        setError({
+          message:
+            error.message ?? "Failed to fetch places, please try again later",
+        });
+        setIsFetching(false);
+      }
+    }
+    fetchPlaces();
+  }, []);
+
+  if (error) {
+    return (
+      <section className="places-category">
+        <h2>Error occured</h2>
+        <p className="fallback-text">{error.message}</p>
+      </section>
+    );
+  }
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
